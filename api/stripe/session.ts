955b45731446122e4be0a56a9796d356924f76b7
THEWHITELIST <lucien.benneau@gmail.com>
2026-02-01 23:03:40 +0100

    Update session.ts

--- api/stripe/session.ts
+++ api/stripe/session.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import Stripe from "stripe";
 
 // Initialize Stripe with the secret key from environment variables
@@ -49,4 +49,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     });
   }
 }
+
 
